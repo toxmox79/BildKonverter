0db4cf28dc7e243a88f37b2f5b5e67f1037dc7e0
Thomas Petzold <tomlux@gmx.de>
2025-08-05 16:28:46 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,16 @@
-const CACHE_NAME = 'bild-konverter-cache-v1';
-const OFFLINE_URLS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_URLS))
+self.addEventListener('install', function(e) {
+  console.log('Service Worker installiert');
+  e.waitUntil(
+    caches.open('bild-konverter-cache').then(function(cache) {
+      return cache.addAll(['index.html']);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) =>
-      cached || fetch(event.request)
-    )
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
 });
